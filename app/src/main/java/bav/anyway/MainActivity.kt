baefc9f7efbd1db47af73100ea6791bad43b4eaa
Aleksandr Bakanov <aleksandr_bakanov@epam.com>
2019-06-26 17:26:15 +0300

    Add MainActivity and initial usage of OSM

---
+++ app/src/main/java/bav/anyway/MainActivity.kt
@@ -0,0 +1,39 @@
+package bav.anyway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val ctx = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+
+        setContentView(R.layout.activity_main)
+
+        map.setTileSource(TileSourceFactory.MAPNIK)
+
+        map.setMultiTouchControls(true)
+        val mapController = map.controller
+        mapController.setZoom(9.5)
+        val startPoint = GeoPoint(48.8583, 2.2944)
+        mapController.setCenter(startPoint)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
+    }
+}
